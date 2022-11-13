80a129ab48162adb39a063b9523658bbd7b09ab4
gustavo soto moya <106712601+gusscamp22@users.noreply.github.com>
2022-11-12 23:43:04 -0300

    Update crud.py
    cambios realizados

--- backend/base de datos/crud.py
+++ backend/base de datos/crud.py
@@ -4,13 +4,13 @@ class Conectar():
 	
 			def _init_(self) -> None:
 				try:
-					self..conexion = mysql..connector. connect(
+					self..conexion = mysql.connector. connect(
 					
-							host =  ˆlocalhostˆ ,
+							host =  ´localhost´ ,
 							port = 3306,
-							user  = ˆrootˆ ,
-							password =  ˆcontraseñaBBDDˆ ,
-							db = ˆMiBaseDeDatosˆ
+							user  = ´root´ ,
+							password =  ´contraseñaBBDD´,
+							db = ´MiBaseDeDatos´
 							
 							)
 					except mysql.connector.Error as  descripcionError
@@ -19,58 +19,58 @@ class Conectar():
 				def InsertarRegistro(self):
 					if self.conexion.is_connected():
 						try:
-							cursor =  self.conexion.cursor()
-							usuario= "INSERT INTO registro VALUES(ˆNombreˆ ,"Apellido ", "Mail","Contraseña"")"
-							data= (nombre,apellido,mail,contraseña)
+						    cursor =  self.conexion.cursor()
+						     usuario= "INSERT INTO registro VALUES(ˆNombreˆ ,"Apellido ", "Mail","Contraseña"")"
+						     data= (nombre,apellido,mail,contraseña)
 							
 							cursor.execute(usuario,data)
 							self.conexion.commit()
 							self.conexion.close()
 							
 							except:
-								print( "Conexion realizada" )
+							      print( "Conexion realizada" )
 								
 								
-								def ActualizarRegistro(self):
-									if self.conexion.is_connected():
-										try:
-											cursor= self.conexion.cursor()
-											usuario= "UPDATE  nombre, apellido SET mail,contraseña WHERE id_registro"
-											cursor.execute(usuario)
-											self.conexion.commit()
+				def ActualizarRegistro(self):
+				    if self.conexion.is_connected():
+				       try:
+					cursor= self.conexion.cursor()
+					usuario= "UPDATE  nombre, apellido SET mail,contraseña WHERE id_registro"
+					cursor.execute(usuario)
+					self.conexion.commit()
 											
-											self.conexion.close()
+					self.conexion.close()
 											
 					except:
-								print("Conexion realizada"")
+					       print("Conexion realizada")
 								
-								def BuscarRegistro(self):
-									if self.conexion. is_connected():
-										try:
-											cursor = self.connexion.cursor()
-											usuario= "SELECT  * from registro where  nombre,apellido,mail.contraseña""
-											cursor.execute(usuario)
-											resltadoREAD = cursor.fetchall()
+			def BuscarRegistro(self):
+			   if self.conexion. is_connected():
+			      try:
+				cursor = self.connexion.cursor()
+				usuario= "SELECT  * from registro where  "nombre","apellido","mail","contraseña"
+				cursor.execute(usuario)
+				resltadoREAD = cursor.fetchall()
 											
-										return resultadoREAD
+				return resultadoREAD
 										
-										self.conexion.close()
+				self.conexion.close()
 											
 											
-										except:
-											print("Conexion realizada")
+		 except:
+			print("Conexion realizada")
 											
 											
-						def EliminaRegistro(self,ID):
-							if self.conexion. is_connected():
-								try:
-									cursosr = self.conexion.cursor()
-									usuario =  "DELETE from registro where id= IDregistro""
-									cursor..execute(usuario)
+			def EliminaRegistro(self,ID):
+			   if self.conexion. is_connected():
+			       try:
+				cursosr = self.conexion.cursor()
+				usuario =  "DELETE from registro where id= IDregistro"
+				cursor..execute(usuario)
 									
 									
-									self.connexion.commit()
-									self.conexion.close()
-						except:
-							print("Conexion realizada")
+				self.connexion.commit()
+				self.conexion.close()
+			except:
+				print("Conexion realizada")
 							
